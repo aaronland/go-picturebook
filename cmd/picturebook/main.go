ae5aa1aec1fa21c029ee379f864eb4596fdf6d25
thisisaaronland <devnull@localhost>
2020-09-02 15:46:14 -0700

    start working on issue #8; everything is broken

--- cmd/picturebook/main.go
+++ cmd/picturebook/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/aaronland/go-picturebook"
 	"github.com/aaronland/go-picturebook/caption"
 	"github.com/aaronland/go-picturebook/filter"
@@ -10,6 +11,7 @@ import (
 	"github.com/sfomuseum/go-flags/multi"
 	"log"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -43,17 +45,91 @@ func Picturebook() error {
 	flag.Var(&filter_uris, "filter", "...")
 	flag.Var(&process_uris, "process", "...")
 
-	/*
-		var include multi.MultiRegexp
-		var exclude multi.MultiRegexp
+	// Deprecated flags
 
-		flag.Var(&include, "include", "A valid regular expression to use for testing whether a file should be included in your picturebook.")
-		flag.Var(&exclude, "exclude", "A valid regular expression to use for testing whether a file should be excluded from your picturebook.")
-	*/
+	var preprocess_uris multi.MultiString
+	var include multi.MultiRegexp
+	var exclude multi.MultiRegexp
+
+	flag.Var(&preprocess_uris, "pre-process", "DEPRECATED: Please use -process process://{PROCESS_NAME} instead.")
+	flag.Var(&include, "include", "A valid regular expression to use for testing whether a file should be included in your picturebook. DEPRECATED: Please use -filter regexp://include/?pattern={REGULAR_EXPRESSION} instead.")
+	flag.Var(&exclude, "exclude", "A valid regular expression to use for testing whether a file should be excluded from your picturebook. DEPRECATED: Please use -filter regexp://exclude/?pattern={REGULAR_EXPRESSION} instead.")
+
+	var target = flag.String("target", "", "Valid targets are: cooperhewitt; flickr; orthis. If defined this flag will set the -filter and -caption flags accordingly. DEPRECATED: Please use specific -filter and -caption flags as needed.")
 
 	flag.Parse()
 
 	ctx := context.Background()
+
+	uri_re, err := regexp.Compile(`(?:[a-z0-9_]+):\/\/.*`)
+
+	if err != nil {
+		log.Fatalf("Failed to compile URI regular expression, %v", err)
+	}
+
+	if *target != "" {
+
+		log.Println("WARNING The -target flag is deprecated. Please use specific -filter and -caption flags as needed.")
+
+		str_filter := fmt.Sprintf("%s://", *target)
+		str_caption := fmt.Sprintf("%s://", *target)
+
+		err := filter_uris.Set(str_filter)
+
+		if err != nil {
+			log.Fatalf("Failed to assign filter '%s', %v", str_filter, err)
+		}
+
+		if *caption_uri != "" {
+			log.Fatalf("Can not assign -caption using -target since -caption is already defined.")
+		}
+
+		*caption_uri = str_caption
+	}
+
+	if len(preprocess_uris) > 0 {
+
+		log.Println("WARNING The -pre-process flag is deprecated. Please use -process process://{PROCESS_NAME} flags instead.")
+
+		for _, pr := range preprocess_uris {
+
+			str_process := fmt.Sprintf("%s://", pr)
+			err := process_uris.Set(str_process)
+
+			if err != nil {
+				log.Fatalf("Failed to assign process '%s', %v", str_process, err)
+			}
+		}
+	}
+
+	if len(include) > 0 {
+
+		log.Println("WARNING The -include flag is deprecated. Please use -filter regexp://include?pattern=... flags instead.")
+		for _, re := range include {
+
+			str_filter := fmt.Sprintf("regexp://include?pattern=%s", re.String())
+			err := filter_uris.Set(str_filter)
+
+			if err != nil {
+				log.Fatalf("Failed to assign filter '%s', %v", str_filter, err)
+			}
+		}
+	}
+
+	if len(exclude) > 0 {
+
+		log.Println("WARNING The -exclude flag is deprecated. Please use -filter regexp://exclude?pattern=... flags instead.")
+
+		for _, re := range exclude {
+
+			str_filter := fmt.Sprintf("regexp://exclude?pattern=%s", re.String())
+			err := filter_uris.Set(str_filter)
+
+			if err != nil {
+				log.Fatalf("Failed to assign filter '%s', %v", str_filter, err)
+			}
+		}
+	}
 
 	opts, err := picturebook.NewPictureBookDefaultOptions(ctx)
 
@@ -94,6 +170,10 @@ func Picturebook() error {
 		filters := make([]filter.Filter, len(filter_uris))
 
 		for idx, filter_uri := range filter_uris {
+
+			if !uri_re.MatchString(filter_uri) {
+				filter_uri = fmt.Sprintf("%s://", filter_uri)
+			}
 
 			f, err := filter.NewFilter(ctx, filter_uri)
 
@@ -138,6 +218,10 @@ func Picturebook() error {
 	}
 
 	if *caption_uri != "" {
+
+		if !uri_re.MatchString(*caption_uri) {
+			*caption_uri = fmt.Sprintf("%s://", *caption_uri)
+		}
 
 		c, err := caption.NewCaption(ctx, *caption_uri)
 
