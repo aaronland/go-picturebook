2a18bed0e072bbd407e849a8b80f10b71b3ca000
thisisaaronland <devnull@localhost>
2020-09-03 22:59:57 -0700

    block out orthis sort code, untested

--- sort/orthis.go
+++ sort/orthis.go
@@ -2,16 +2,14 @@ package sort
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"github.com/aaronland/go-picturebook/picture"
-	_ "github.com/tidwall/gjson"
-	_ "io/ioutil"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
 	"net/url"
-	_ "os"
-	_ "path/filepath"
-	_ "strconv"
-	_ "strings"
-	_ "time"
+	"os"
+	"path/filepath"
+	"sort"
 )
 
 func init() {
@@ -42,40 +40,60 @@ func NewOrThisSorter(ctx context.Context, uri string) (Sorter, error) {
 
 func (f *OrThisSorter) Sort(ctx context.Context, pictures []*picture.PictureBookPicture) error {
 
-	return errors.New("Not implemented")
+	lookup := make(map[string]*picture.PictureBookPicture)
+	candidates := make([]string, 0)
 
-	/*
-			root := filepath.Dir(path)
-			root = filepath.Dir(root)
+	for _, pic := range pictures {
 
-			index := filepath.Join(root, "index.json")
+		path := pic.Path
 
-			fh, err := os.Open(index)
+		root := filepath.Dir(path)
+		root = filepath.Dir(root)
 
-			if err != nil {
-				return false, err
-			}
+		index := filepath.Join(root, "index.json")
 
-			defer fh.Close()
+		fh, err := os.Open(index)
 
-			body, err := ioutil.ReadAll(fh)
+		if err != nil {
+			return err
+		}
 
-			if err != nil {
-				return false, err
-			}
+		defer fh.Close()
 
-			date_rsp := gjson.GetBytes(body, "date")
+		body, err := ioutil.ReadAll(fh)
 
-			if !date_rsp.Exists() {
-				return false, nil
-			}
+		if err != nil {
+			return err
+		}
 
-			str_year := strconv.Itoa(f.year)
+		id_rsp := gjson.GetBytes(body, "id")
 
-			if !strings.HasPrefix(date_rsp.String(), str_year) {
-				return false, nil
-			}
+		if !id_rsp.Exists() {
+			continue
+		}
 
-		return true, nil
-	*/
+		date_rsp := gjson.GetBytes(body, "date")
+
+		if !date_rsp.Exists() {
+			continue
+		}
+
+		key := fmt.Sprintf("%s-%s", date_rsp.String(), id_rsp.String())
+		lookup[key] = pic
+	}
+
+	for key, _ := range lookup {
+		candidates = append(candidates, key)
+	}
+
+	sort.Strings(candidates)
+
+	sorted := make([]*picture.PictureBookPicture, len(candidates))
+
+	for idx, key := range candidates {
+		sorted[idx] = lookup[key]
+	}
+
+	pictures = sorted
+	return nil
 }
