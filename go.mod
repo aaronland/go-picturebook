662bb77ef006f537b286bcffd29c794706dfa59f
thisisaaronland <devnull@localhost>
2020-09-27 22:05:38 -0700

    remove bespoke code; update vendor deps

--- go.mod
+++ go.mod
@@ -7,13 +7,12 @@ require (
 	github.com/aaronland/go-image-rotate v0.0.3
 	github.com/aaronland/go-image-tools v0.1.3
 	github.com/aaronland/go-roster v0.0.2
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.1.2
 	github.com/jung-kurt/gofpdf v1.16.2
 	github.com/microcosm-cc/exifutil v0.0.0-20140910154058-36de169162e2
 	github.com/rainycape/unidecode v0.0.0-20150907023854-cb7f23ec59be
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
-	github.com/sfomuseum/go-flags v0.4.1
+	github.com/sfomuseum/go-flags v0.4.2
 	github.com/sfomuseum/go-font-ocra v0.0.1
-	github.com/tidwall/gjson v1.6.1
 	gocloud.dev v0.20.0
 )
---
+++ vendor/github.com/sfomuseum/go-flags/multi/keyvalue.go
@@ -0,0 +1,47 @@
+package multi
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type KeyValueFlag struct {
+	Key   string
+	Value string
+}
+
+type KeyValue []*KeyValueFlag
+
+func (e *KeyValue) String() string {
+	return fmt.Sprintf("%v", *e)
+}
+
+func (e *KeyValue) Set(value string) error {
+
+	value = strings.Trim(value, " ")
+	kv := strings.Split(value, "=")
+
+	if len(kv) != 2 {
+		return errors.New("Invalid key=value argument")
+	}
+
+	a := KeyValueFlag{
+		Key:   kv[0],
+		Value: kv[1],
+	}
+
+	*e = append(*e, &a)
+	return nil
+}
+
+func (e *KeyValue) ToMap() map[string]string {
+
+	m := make(map[string]string)
+
+	for _, arg := range *e {
+		m[arg.Key] = arg.Value
+	}
+
+	return m
+}
