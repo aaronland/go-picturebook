e5fe912efd81cc39afd67ca30795c2b76db94ae5
thisisaaronland <devnull@localhost>
2021-02-23 09:41:03 -0800

    ensure that source_bucket = target_bucket, when necessary, in process/multi.go per issue #12; notes to self

--- picture/picture.go
+++ picture/picture.go
@@ -8,5 +8,5 @@ type PictureBookPicture struct {
 	Source  string
 	Path    string
 	Caption string
-	Bucket  *blob.Bucket
+	Bucket  *blob.Bucket	// right now this is optional but may become required in the future, TBD.
 }
--- process/multi.go
+++ process/multi.go
@@ -31,8 +31,15 @@ func (p *MultiProcess) Transform(ctx context.Context, source_bucket *blob.Bucket
 			return "", err
 		}
 
-		if new_path != "" {
+		if new_path != "" && new_path != path {
 			final_path = new_path
+
+			// See this - it's important. Because we're in a loop we need to make sure
+			// we know where to find the second (and third...) temporary file that's been
+			// processed which will be in the "target" (or temporary) bucket and not the
+			// source bucket. (20210223/straup)
+			
+			source_bucket = target_bucket
 		}
 	}
 
